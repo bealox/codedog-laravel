ed6292b3929cce95974038b74f120e48d8341b75
bealox <thebealox@gmail.com>
2014-12-29 23:47:21 +1100

    changes

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -5,12 +5,9 @@
 <title>@yield('meta_title', 'seekdog')</title>
 {{HTML::script('packages/jquery-1.11.2.min.js')}}
 
-<!-- Pure -->
-<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
 <!-- Font-awsome -->
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<!-- bootstrap -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
